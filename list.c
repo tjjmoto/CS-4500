9bf1939ce9183ec3f7b4b672773b7b75c4ba4d27
Troy Joslyn <tjjmoto@gmail.com>
2023-01-21 13:16:46 -0700

    made createlist and addtolist

--- list.c
+++ list.c
@@ -3,6 +3,10 @@
 #include <string.h>
 #include "list.h"
 
+node *head;
+node *current = NULL;
+list *llist;
+
 int main(){
 
 }
@@ -10,38 +14,55 @@ int main(){
 /* Allocate space for a new list and set its head to NULL.
 Returns the created list if successful , NULL otherwise . */
 list* createlist() {
+    // TODO: Allocate space for new list
+    head = malloc(sizeof(llist));
+    llist->head = head;
 
+    if (llist->head != NULL) {
+        return llist;
+    } else {
+        return NULL;
+    }
 };
 
-/* Allocates a new node and copies the s t r i n g from item to t h i s node
-18 ∗ ( use malloc , strlen , and strncpy ; or try strdup ) . Adds t h i s new node
-19 ∗ to end of the l i s t l l . Returns 0 i f successful , non−zero otherwise . */
+/* Allocates a new node and copies the string from item to this node
+( use malloc , strlen , and strncpy ; or try strdup ) . Adds this new node
+to end of the list ll . Returns 0 if successful , non−zero otherwise . */
 int addtolist(list* ll, char* item) {
+    current = malloc(sizeof(ll));
+    current->item = item;
 
+    if (current->item == ll->head->item) {
+        // Successful
+        return 0;
+    } else {
+        // Not successful
+        return 1;
+    }
 };
 
-/* Removes the head of the l i s t l l , extracts the s t r i n g stored in the head ,
-23 ∗ and returns a pointer to t h i s s t r i n g . Also f r e e s the removed head node . */
+/* Removes the head of the list ll , extracts the string stored in the head ,
+and returns a pointer to thisstring . Also frees the removed head node . */
 char* removefromlist(list* ll) {
 
 };
 
-/* Prints every s t r i n g in each node of the l i s t l l , with a new l i n e
-∗ character at the end of each s t r i n g */
+/* Prints every string in each node of the list ll , with a new line
+character at the end of each string */
 void printlist(list* ll) {
 
 };
 
-/* Flushes ( c l e a r s ) the e n t i r e l i s t and re−i n i t i a l i z e s the l i s t . The passed
-∗ pointer l l should s t i l l point to a valid , empty l i s t when t h i s function
-∗ returns . Any memory a l l o c a t e d to store nodes in the l i s t should be freed .
+/* Flushes (clears) the entirlist and re−initializes the list . The passed
+∗ pointer ll should still point to a valid , empty list when this function
+∗ returns . Any memory allocated to store nodes in the list should be freed .
 */
 void flushlist(list* ll) {
 
 };
 
-/* De−a l l o c a t e s a l l data f o r the l i s t . Ensure a l l memory a l l o c a t e d f o r l i s t
-l l i s freed , including any a l l o c a t e d s t r i n g s and l i s t l l i t s e l f . */
+/* De−allocates all data for the list . Ensure all memory allocated for list
+ll is freed , including any al located strings and list ll itself . */
 void freelist (list* ll) {
 
 };

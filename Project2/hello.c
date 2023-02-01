debdcb535f107adba8787e4e2ef1d816be5a79fd
Troy Joslyn <tjjmoto@gmail.com>
2023-02-01 08:16:50 -0700

    fixed code issues not includes

--- Project2/hello.c
+++ Project2/hello.c
@@ -1,18 +1,18 @@
-#include <linux /module . h>
-#include <linux / kernel . h>
+#include <linux/module.h>
+#include <linux/kernel.h>
 
-int init module ( void ) 
+int init module (void) 
 {
-	printk (KERN_INFO "Hello world !\ n" ) ;
+	printk (KERN_INFO "Hello world !\n") ;
 	return 0;
 }
 
-void cleanup module ( void )
+void cleanup module (void)
 {
-	printk (KERN_INFO "Goodbye world !\ n" ) ;
+	printk (KERN_INFO "Goodbye world !\n") ;
 }
 
-module_init ( init_module ) ;
-module_exit ( cleanup_module ) ;
+module_init (init_module) ;
+module_exit (cleanup_module) ;
 
-MODULE_LICENSE( ”GPL” ) ;
+MODULE_LICENSE("GPL") ;

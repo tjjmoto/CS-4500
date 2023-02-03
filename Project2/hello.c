4d8f091e0de911d3f347ad46a49c645c17dd4849
SKettner <skettner@uccs.edu>
2023-02-03 15:40:03 -0700

    Update hello.c
    Removed some white space.

--- Project2/hello.c
+++ Project2/hello.c
@@ -3,16 +3,16 @@
 
 int init module (void) 
 {
-	printk (KERN_INFO "Hello world !\n") ;
+	printk (KERN_INFO "Hello world !\n");
 	return 0;
 }
 
 void cleanup module (void)
 {
-	printk (KERN_INFO "Goodbye world !\n") ;
+	printk (KERN_INFO "Goodbye world !\n");
 }
 
-module_init (init_module) ;
-module_exit (cleanup_module) ;
+module_init (init_module);
+module_exit (cleanup_module);
 
-MODULE_LICENSE("GPL") ;
+MODULE_LICENSE("GPL");

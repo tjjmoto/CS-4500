8fcc58136816994cf0ff4691649b7f080e6eafb8
SKettner <skettner@uccs.edu>
2023-02-03 15:44:42 -0700

    Update hello.c
    Added two underscores I missed.

--- Project2/hello.c
+++ Project2/hello.c
@@ -1,13 +1,13 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
-int init module (void) 
+int init_module (void) 
 {
 	printk (KERN_INFO "Hello world !\n");
 	return 0;
 }
 
-void cleanup module (void)
+void cleanup_module (void)
 {
 	printk (KERN_INFO "Goodbye world !\n");
 }

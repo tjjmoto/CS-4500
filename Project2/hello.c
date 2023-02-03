a8ca95009f5b294874a345cac437493c5cb5631c
SKettner <skettner@uccs.edu>
2023-02-03 16:02:34 -0700

    Update hello.c
    Trying name change

--- Project2/hello.c
+++ Project2/hello.c
@@ -1,18 +1,18 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
-int init_module(void) 
+int init_module2(void) 
 {
 	printk(KERN_INFO "Hello world !\n");
 	return 0;
 }
 
-void cleanup_module(void)
+void cleanup_module2(void)
 {
 	printk(KERN_INFO "Goodbye world !\n");
 }
 
-module_init(init_module);
-module_exit(cleanup_module);
+module_init(init_module2);
+module_exit(cleanup_module2);
 
 MODULE_LICENSE("GPL");

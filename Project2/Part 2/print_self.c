fccdb88487935992ef75bebb41444c0dab62cf9f
Troy Joslyn <tjjmoto@gmail.com>
2023-02-04 08:09:12 -0700

    added child process

--- Project2/Part 2/print_self.c
+++ Project2/Part 2/print_self.c
@@ -5,5 +5,18 @@
 int self_init_module(void) {
     struct task_struct *task;
 
-    printk(KERN_INFO "Child Process name: %s\n", current->comm);
+    printk(KERN_INFO "Child Process Name: %s\n", current->comm);
+    printk(KERN_INFO "Child Process ID: %d\n", current->pid);
+    printk(KERN_INFO "Child Process State: ");
+    switch(current->state) {
+        case -1:
+            printk("can't run\n");
+            break;
+        case 0:
+            printk("can run\n");
+            break;
+        default: 
+            printk("stopped\n");
+            break;
+    }
 }

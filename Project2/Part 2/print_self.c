9d9be56f828a704374fd8125619f318679838557
Troy Joslyn <tjjmoto@gmail.com>
2023-02-04 08:20:14 -0700

    parent task and cleanup

--- Project2/Part 2/print_self.c
+++ Project2/Part 2/print_self.c
@@ -8,15 +8,23 @@ int self_init_module(void) {
     printk(KERN_INFO "Child Process Name: %s\n", current->comm);
     printk(KERN_INFO "Child Process ID: %d\n", current->pid);
     printk(KERN_INFO "Child Process State: ");
-    switch(current->state) {
-        case -1:
-            printk("can't run\n");
-            break;
-        case 0:
-            printk("can run\n");
-            break;
-        default: 
-            printk("stopped\n");
-            break;
+
+    if (current->state == -1) {
+        printk("TASK_UNRUNNABLE\n");
+    } else if (current->state == 0) {
+        printk("TASK_RUNNING\n");
+    } else {
+        printk("TASK_STOPPED\n");
     }
+
+    for (task=current; task!=&init_task; task=task->parent) {
+        printk(KERN_INFO "Parent Process Name: %s\n", task->comm);
+        printk(KERN_INFO "Parent Process ID: %d\n", task->pid);
+    }
+
+    return 0;
+}
+
+void self_cleanup_module(void) {
+    printk(KERN_INFO "Clean up\n");
 }

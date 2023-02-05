b8ccd81c73198125dff102891302393fd0a35e92
Troy Joslyn <tjjmoto@gmail.com>
2023-02-05 16:00:32 -0700

    testing print other

--- Project2/Part3/print_other.c
+++ Project2/Part3/print_other.c
@@ -7,15 +7,15 @@ int self_init_module(void) {
 
     printk(KERN_INFO "Child Process Name: %s\n", current->comm);
     printk(KERN_INFO "Child Process ID: %d\n", current->pid);
-    printk(KERN_INFO "Child Process State: ");
+    printk(KERN_INFO "Child Process State: %s", "Test");
 
-    if (current->state == -1) {
-        printk("TASK_UNRUNNABLE\n");
-    } else if (current->state == 0) {
-        printk("TASK_RUNNING\n");
-    } else {
-        printk("TASK_STOPPED\n");
-    }
+    // if (current->state == -1) {
+    //     printk("TASK_UNRUNNABLE\n");
+    // } else if (current->state == 0) {
+    //     printk("TASK_RUNNING\n");
+    // } else {
+    //     printk("TASK_STOPPED\n");
+    // }
 
     for (task=current; task!=&init_task; task=task->parent) {
         printk(KERN_INFO "Parent Process Name: %s\n", task->comm);

b187ffde41df62528286577e2718cf5c4f4346e1
SKettner <skettner@uccs.edu>
2023-02-07 14:15:40 -0700

    Added code that will hopefully make part 3 work.

--- Project2/Part3/print_other.c
+++ Project2/Part3/print_other.c
@@ -3,15 +3,14 @@
 #include <linux/sched.h>
 #include <string.h>
 
-int other_init_module(void) {
+int other_init_module(task_struct *task) {
     struct task_struct *task;
-    struct task_struct *temp;
     char childState[25];
 
-    if (current->state == -1) {
+    if (task->state == -1) {
         printk("TASK_UNRUNNABLE\n");
         childState = "TASK_UNRUNNABLE";
-    } else if (current->state == 0) {
+    } else if (task->state == 0) {
         printk("TASK_RUNNING\n");
         childState = "TASK_RUNNING";
     } else {
@@ -19,9 +18,14 @@ int other_init_module(void) {
         childState = "TASK_STOPPED";
     }
 
-    printk(KERN_INFO "Child Process Name: %s\n", current->comm);
-    printk(KERN_INFO "Child Process ID: %d\n", current->pid);
+    printk(KERN_INFO "Child Process Name: %s\n", task->comm);
+    printk(KERN_INFO "Child Process ID: %d\n", task->pid);
     printk(KERN_INFO "Child Process State: %s\n", childState);
+    
+    if(task!=&init_task)
+	{
+		task=task->parent;
+	}
 
     for (task=current; task!=&init_task; task=task->parent) {
         printk(KERN_INFO "Parent Process Name: %s\n", task->comm);

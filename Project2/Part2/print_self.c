f40c47440456ce72b453957d25f3b0777c04f0d4
Troy Joslyn <tjjmoto@gmail.com>
2023-02-08 14:13:46 -0700

    fix error print_self

--- Project2/Part2/print_self.c
+++ Project2/Part2/print_self.c
@@ -4,22 +4,18 @@
 
 int self_init_module(void) {
     struct task_struct *task;
-    char childState[25];
-
-    if (current->state == -1) {
-        printk("TASK_UNRUNNABLE\n");
-        childState = "TASK_UNRUNNABLE";
-    } else if (current->state == 0) {
-        printk("TASK_RUNNING\n");
-        childState = "TASK_RUNNING";
-    } else {
-        printk("TASK_STOPPED\n");
-        childState = "TASK_STOPPED";
-    }
 
     printk(KERN_INFO "Child Process Name: %s\n", current->comm);
     printk(KERN_INFO "Child Process ID: %d\n", current->pid);
-    printk(KERN_INFO "Child Process State: %s\n", childState);
+    printk(KERN_INFO "Child Process State: ");
+
+    if (current->state == -1) {
+        printk("TASK_UNRUNNABLE\n");
+    } else if (current->state == 0) {
+        printk("TASK_RUNNING\n");
+    } else {
+        printk("TASK_STOPPED\n");
+    }
 
     for (task=current; task!=&init_task; task=task->parent) {
         printk(KERN_INFO "Parent Process Name: %s\n", task->comm);

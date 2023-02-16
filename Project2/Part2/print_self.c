d85a82af87becb8883e00330b875cecb0214ef5b
Troy Joslyn <tjjmoto@gmail.com>
2023-02-16 10:40:39 -0700

    comments and clean up

--- Project2/Part2/print_self.c
+++ Project2/Part2/print_self.c
@@ -5,10 +5,12 @@
 int self_init_module(void) {
     struct task_struct *task;
 
+    /* Prints all child and parent processes that result from print_self until init*/
     printk(KERN_INFO "Child Process Name: %s\n", current->comm);
     printk(KERN_INFO "Child Process ID: %d\n", current->pid);
     printk(KERN_INFO "Child Process State: ");
 
+    // Prints the state of the child process based on the state ID sent to the machine.
     if (current->state == -1) {
         printk("TASK_UNRUNNABLE\n");
     } else if (current->state == 0) {
@@ -17,6 +19,7 @@ int self_init_module(void) {
         printk("TASK_STOPPED\n");
     }
 
+    // Prints every remaining parent process that is running unit init
     for (task=current; task!=&init_task; task=task->parent) {
         printk(KERN_INFO "Parent Process Name: %s\n", task->comm);
         printk(KERN_INFO "Parent Process ID: %d\n", task->pid);
--- Project2/Part3/print_other.c
+++ Project2/Part3/print_other.c
@@ -5,18 +5,19 @@
 
 int other_init_module(int pid) {
     struct task_struct *task;
-
-    //Tried to get pid to work but won't work for some reason
-    //task = pid_task(find_vpid(pid), PIDTYPE_PID);
 	
+	// Setting the current task to the task struct
 	task = current; 
 
+	/* Checks to see if the process pid is the process that is input then prints out the details
+	 that pertain to the specific task PID that was sent into the module.*/
 	if(task->pid != pid)
 	{
 		printk(KERN_INFO "Child Process Name: %s\n", task->comm);
 		printk(KERN_INFO "Child Process ID: %d\n", task->pid);
 		printk(KERN_INFO "Child Process State: ");
 		
+		// Prints the state of the child process based on the state ID sent to the machine.
 		if (task->state == -1) {
 		    printk("TASK_UNRUNNABLE\n");
 		} else if (task->state == 0) {
@@ -30,6 +31,7 @@ int other_init_module(int pid) {
 			task=task->parent;
 		}
 		
+		// Prints every remaining parent process that is running unit init
 		for (task=current; task!=&init_task; task=task->parent) {
 		    printk(KERN_INFO "Parent Process Name: %s\n", task->comm);
 		    printk(KERN_INFO "Parent Process ID: %d\n", task->pid);

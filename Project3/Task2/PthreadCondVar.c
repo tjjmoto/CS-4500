08562da82cb85aa8e42500ba0ec71b67605897a3
Troy Joslyn <tjjmoto@gmail.com>
2023-04-15 07:22:58 -0600

    Reorganized task 2

--- Project3/Task2/PthreadCondVar.c
+++ Project3/Task2/PthreadCondVar.c
@@ -1,57 +1,39 @@
-// TODO: CHANGE EVERYTHING
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 char queue [5] ; 
-int front = 0 , rear = 0 ; // pointrs to front and rear of the queu
-int items = 0 ; // number of items in queu
-int done = 1 ; // to show that the input file is finished to read
+int front = 0 , rear = 0 ;
+int items = 0 ; 
+int fileRead = 1 ;
 
 pthread_mutex_t mutex ;
-pthread_cond_t item_available ; // condition variable to show producer put a word in queu
-pthread_cond_t space_available ; // condition variable to show consumer delete a word from queu
+pthread_cond_t item_available ;
+pthread_cond_t space_available ;
 
-void * p_thread() ; // the producer
-void * c_thread() ; // the consumer
+// Function Prototypes
+void prod_thread();
+void cons_thread();
 
 int main(int argc, char *argv[])
 {
-
-	pthread_t producer , consumer;
-	//pthread_init();
+	// P thread variables for execution
+	pthread_t p_prod , p_cons;
 	pthread_cond_init (&item_available, NULL) ;
 	pthread_cond_init (&space_available, NULL) ;
 	pthread_mutex_init (&mutex, NULL) ;
 	
-	if( pthread_create ( &producer , NULL, p_thread, NULL))  // create producer
-	{
-		fprintf(stderr, "Error creating producer thread\n");
-		return 1;
-	}
+	// Creating the pthread instance for producers
+	pthread_create ( &p_prod , NULL, prod_thread, NULL);
 
-	if( pthread_create ( &consumer , NULL, c_thread, NULL))  // create the consumer
-	{
-		fprintf(stderr, "Error creating consumer thread\n");
-		return 1;
-	}
-	
-	if ( pthread_join ( producer , NULL ) ) // wait for all threads to finish
-	{
-		fprintf(stderr, "Error joining thread\n");
-		return 2;
-	}
-
-	if ( pthread_join ( consumer , NULL ) ) 
-	{
-		fprintf(stderr, "Error joining consumer\n");
-		return 2;
-	}	
-	
+	// Creating the pthread instance for consumers
+	pthread_create ( &p_cons , NULL, cons_thread, NULL);
+	pthread_join ( p_prod , NULL );
+	pthread_join ( p_cons , NULL );
 	return 0;
 	
 }
-void * p_thread()
+void prod_thread()
 {	
 	FILE *fp = fopen("message.txt","r");
 	char c ;
@@ -61,11 +43,11 @@ void * p_thread()
 		sleep(1);
 		pthread_mutex_lock (&mutex) ;
 			//2
-			printf ("	front<%d> rear<%d> items <%d>\n", front , rear , items ) ;	
+			printf ("	front<%d> rear<%d>\n", front , rear) ;	
 			while ( items >= 5 ) // while there is no space in queu to write
 				pthread_cond_wait ( &space_available , &mutex ) ;
 			//3	
-			printf ("	front<%d> rear<%d> items <%d>\n", front , rear , items ) ;
+			printf ("	front<%d> rear<%d>\n", front , rear) ;
 			
 			// now we cAN write in queue
 			queue [front] = c ;
@@ -81,45 +63,36 @@ void * p_thread()
 		c = getc(fp);		
 	}
 	pthread_mutex_lock (&mutex) ;
-	done = 0 ; // we should tell the consumer that the file is finished
+	fileRead = 0 ; // we should tell the consumer that the file is finished
 	pthread_cond_signal(&item_available);
 	pthread_mutex_unlock (&mutex) ;
 	
 	fclose (fp);
-
-	
-	//printf ("hello prof. Song Jiang\n") ;
-	//return NULL;		
-	 pthread_exit (0);
+	pthread_exit (0);
 }
 
-void * c_thread()
+void cons_thread()
 {	
-	FILE* output = fopen ("message_result.txt" , "w") ;
-	//4
-	while ( done != 0 ) { // while there is something to read
-		pthread_mutex_lock (&mutex) ;
-			//5
-			printf ("front<%d> rear<%d> items <%d>\n", front , rear , items ) ;	
-			while ( items <= 0 && done!= 0 ) // while there is no character in queu to read we should wait
+	while ( fileRead != 0 ) {
+		pthread_mutex_lock (&mutex);
+			printf ("front<%d> rear<%d>\n", front , rear );	
+
+			while ( items <= 0 && fileRead!= 0 ) {
 				pthread_cond_wait ( &item_available , &mutex ) ;
-			//6
-			printf ("front<%d> rear<%d> items <%d>\n", front , rear , items ) ;	
-				
-			// read the character and write it:
+			}
+
+			printf ("front<%d> rear<%d>\n", front , rear );	
+
 			char c = queue [rear] ;
 			rear ++ ;
 			if (rear==5) rear = 0 ;
 			items -- ;
-			printf ("character read from queue: <%c>\n" , c ) ;
-			printf ("wake up a producer \n" ) ;
-			fprintf(output,"%c",c) ;
-			pthread_cond_signal(&space_available); // send signal for producer to show that thre is space
+			printf ("character read from queue: <%c>\n" , c );
+			printf ("wake up a producer \n" );
+			pthread_cond_signal(&space_available);
 		
-		pthread_mutex_unlock (&mutex) ;	
+		pthread_mutex_unlock (&mutex);	
 	}
-	
-	fclose (output) ;
-	//return NULL;
+
 	pthread_exit (0);
 }
